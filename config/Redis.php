624efa8ba7e8078fb32d3da6e029b4c4b61f90e4
Natacha <103414607+NatachaEt@users.noreply.github.com>
2024-07-18 18:10:54 +0200

    Update Redis.php

--- config/Redis.php
+++ config/Redis.php
@@ -3,9 +3,9 @@
 namespace Config;
 
 class Redis {
-    private static $host = 'redis-10087.c323.us-east-1-2.ec2.cloud.redislabs.com:10087';
+    private static $host = '';
     private static $port = 6379;
-    private static $password = 'MRBPq776KvNXRCmV2O4iSwx7KCRuJNV8';
+    private static $password = '';
 
     public static function getHost() {
         return self::$host;
